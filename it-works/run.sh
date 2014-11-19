b2c17b6d2c787d0111f5c29510949dddedd47d50
ferdaousse <ferdaousse.chakir@gmail.com>
2014-11-19 12:03:58 +0100

    un exemple qui marche

---
+++ it-works/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Load custom settings written by bootlocal.sh
+source /home/docker/.profile
+
+# clean
+docker kill $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+#alais
+alias dfig="docker run -ti -v \$(pwd):/app -v /vagrant:/vagrant -v /var/run/docker.sock:/var/run/docker.sock dduportal/fig"
+# start infra.
+dfig up -d 
+sleep 5
+
+# Load data set
+dfig run dbserver psql -h dbserver -p 5432 -U postgres -f /vagrant/simple-db/comptes.sql
+dfig run dbgeoserver -p 8080
